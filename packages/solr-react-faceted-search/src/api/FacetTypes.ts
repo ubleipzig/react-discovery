2c4a796f67ed6f552c179b14a036cecd7db7760e
Christopher Hanna Johnson <chjohnson39@gmail.com>
2019-05-21 10:55:11 +0200

    use queryString stringify to build params (#2)
    add api tests

---
+++ packages/solr-react-faceted-search/src/api/FacetTypes.ts
@@ -0,0 +1,5 @@
+export enum FacetTypes {
+  LIST_FACET = 'list-facet',
+  RANGE_FACET = 'range-facet',
+  PERIOD_RANGE_FACET = 'period-range-facet'
+}
--- packages/solr-react-faceted-search/src/api/extendedDisMaxQueryBuilder.ts
+++ packages/solr-react-faceted-search/src/api/extendedDisMaxQueryBuilder.ts
@@ -1,93 +1,98 @@
 import {IEMaxQuery} from "."
 import {SolrParameters} from "./SolrParameters"
+import {FacetTypes} from "./FacetTypes"
+const queryString = require('query-string')
 
-export const buildDisMaxQuery = (searchFields, stringInput): string => {
+const EDISMAX = "edismax"
+export const buildDisMaxQuery = (searchFields, stringInput): {} => {
   const qfList = searchFields.map((searchField): string => {
     return searchField.field
   }).join(" ")
   if (stringInput) {
-    return `${SolrParameters.TYPE_DEF}=edismax`
-    + `&${SolrParameters.QUERY}=${encodeURIComponent(stringInput)}`
-    + `&${SolrParameters.QF}=${qfList}`
+    return {
+      [SolrParameters.TYPE_DEF]: EDISMAX,
+      [SolrParameters.QUERY]: stringInput,
+      [SolrParameters.QF]: qfList
+    }
   } else {
-    return `${SolrParameters.TYPE_DEF}=edismax`
-    + `&${SolrParameters.QUERY}=*`
-    + `&${SolrParameters.QF}=${qfList}`
+    return {
+      [SolrParameters.TYPE_DEF]: EDISMAX,
+      [SolrParameters.QUERY]: '*',
+      [SolrParameters.QF]: qfList
+    }
   }
 }
 
-export const buildFacetFieldParams = (fields): string => {
+export const buildFacetFieldParams = (fields): any => {
   const ff = fields
-    .filter((field): boolean => field.type === "list-facet" || field.type === "range-facet")
-    .map((field): string => `${SolrParameters.FACET_FIELD}=${encodeURIComponent(field.field)}`)
-    .concat(
-      fields
-        .filter((field): boolean => field.type === "period-range-facet")
-        .map((field): string => `${SolrParameters.FACET_FIELD}=${encodeURIComponent(field.lowerBound)}` +
-          `&${SolrParameters.FACET_FIELD}=${encodeURIComponent(field.upperBound)}`)
-    )
-    .join('&');
-  return ff.length ? `&${ff}` : ""
+    .filter((field): boolean => field.type === FacetTypes.LIST_FACET || field.type === FacetTypes.RANGE_FACET)
+    .map((field): any => field.field)
+  return fields.length ? {[SolrParameters.FACET_FIELD]: ff} : ""
 }
 
-export const buildFilterQueryParams = (filters): string => {
+export const buildFilterQueryParams = (filters): any => {
   const qf = Object.entries(filters)
-    .map(([k, values]): string => (values as [])
-      .map((val): string => `${SolrParameters.FILTER_QUERY}=${k}:"${val}"`)
-      .join('&')).join('&')
-  return qf.length ? `&${qf}` : ""
+    .filter(([ {}, values]: any): boolean => values.length > 0)
+    .map(([k, values]): any => (values as [])
+      .map((val): string => `${k}:"${val}"`))
+  const flattened = [].concat(...qf)
+  return flattened.length ? {[SolrParameters.FILTER_QUERY]: flattened} : ""
 }
 
-export const buildFacetSortParams = (facetSort = "index"): string => {
-  return `&${SolrParameters.FACET_SORT}=${facetSort}`
+export const buildFacetSortParams = (facetSort = "index"): any => {
+  return {[SolrParameters.FACET_SORT]: facetSort}
 }
 
-export const buildFacetLimitParams = (facetLimit = -1): string => {
-  return `&${SolrParameters.FACET_LIMIT}=${facetLimit}`
+export const buildFacetLimitParams = (facetLimit = -1): any => {
+  return {[SolrParameters.FACET_LIMIT]: facetLimit}
 }
 
-export const buildSortParams = (sortFields): string => {
+export const buildSortParams = (sortFields): any => {
   const sf = sortFields
     .filter((sortField, i): boolean => sortField.isSelected || i === 0)
-    .map((sortField): string => encodeURIComponent(`${sortField.field} ${sortField.order}`))
+    .map((sortField): string => `${sortField.field} ${sortField.order}`)
     .join(",");
-  return sf.length ? `&${SolrParameters.SORT}=${sf}` : ""
+  return sf.length ? {[SolrParameters.SORT]: sf} : ""
 }
 
-export const buildSize = (size: number): string => {
-  return `&${SolrParameters.ROWS}=${size}`
+export const buildSize = (size: number): any => {
+  return {[SolrParameters.ROWS]: size}
 }
 
-export const buildGroupFieldParams = (group: boolean, groupField: string): string => {
-  return groupField ? `&${SolrParameters.GROUP}=${group}`
-    + `&${SolrParameters.GROUP_FIELD}=${encodeURIComponent(groupField)}` : ""
+export const buildGroupFieldParams = (group: boolean, groupField: string): any => {
+  return groupField ? {
+    [SolrParameters.GROUP]: group,
+    [SolrParameters.GROUP_FIELD]: groupField} : ""
 }
 
-export const buildHighlighting = (highlighting): string => {
-  return highlighting ? `&${SolrParameters.HL}=${true}&${SolrParameters.HL_FIELDS}=*` : "";
+export const buildHighlighting = (highlighting): any => {
+  return highlighting ? {
+    [SolrParameters.HL]: true,
+    [SolrParameters.HL_FIELDS]: '*'} : "";
 }
 
-export const buildStart = (start: number): string => {
-  return `&${SolrParameters.START}=${start}`
+export const buildStart = (start: number): any => {
+  return {[SolrParameters.START]: start}
 }
 
-export const buildIsFaceted = (facet: boolean = true): string => {
-  return `&${SolrParameters.FACET}=${facet}`
+export const buildIsFaceted = (facet: boolean = true): any => {
+  return {[SolrParameters.FACET]: facet}
 }
 
 export const extendedDisMaxQueryBuilder = (props: IEMaxQuery): string => {
   const {facetLimit, facetSort, filters, group, groupField, highlighting,
     searchFields, sortFields, stringInput, size, start, url} = props
-  const queryString = buildDisMaxQuery(searchFields, stringInput)
-    + buildSortParams(sortFields)
-    + buildFacetFieldParams(searchFields)
-    + buildGroupFieldParams(group, groupField)
-    + buildSize(size)
-    + buildFacetLimitParams(facetLimit)
-    + buildFacetSortParams(facetSort)
-    + buildFilterQueryParams(filters)
-    + buildStart(start)
-    + buildIsFaceted(true)
-    + buildHighlighting(highlighting)
-  return `${url}${SolrParameters.QUERY_CONTEXT}?${queryString}`
+  const qs = {
+    ...buildDisMaxQuery(searchFields, stringInput),
+    ...buildSortParams(sortFields),
+    ...buildFacetFieldParams(searchFields),
+    ...buildGroupFieldParams(group, groupField),
+    ...buildSize(size),
+    ...buildFacetLimitParams(facetLimit),
+    ...buildFacetSortParams(facetSort),
+    ...buildFilterQueryParams(filters),
+    ...buildStart(start),
+    ...buildIsFaceted(true),
+    ...buildHighlighting(highlighting)}
+  return `${url}${SolrParameters.QUERY_CONTEXT}?${queryString.stringify(qs)}`
 }
